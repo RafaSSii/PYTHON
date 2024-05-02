54b303f4d1b66df8209b71c419b747aed8a73aa0
Rafael Santos <149257772+RafaSSii@users.noreply.github.com>
2024-05-02 20:01:35 -0300

    Add files via upload

---
+++ Temperature/main.py
@@ -0,0 +1,7 @@
+f = float(input("Qual a temperatura? "))
+
+celsius = (f-32)*5/9
+kelvin = (f-32)*5/9 + 273.15
+
+print(f"A temperatura em Celsius é: {celsius}")
+print(f"A temperatura em Kelvin é: {kelvin}")
